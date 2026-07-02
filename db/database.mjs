88092c17ca5a7c6543b7e74a8770fa8fc31183e7
고진서 <kohjinseo@gmail.com>
2026-07-02 12:13:34 +0900

    X서버 초기세팅

---
+++ db/database.mjs
@@ -0,0 +1,11 @@
+import MongoDB from "mongodb"
+import {config} from "../config.mjs"
+
+let db
+
+export async function connectDB() {
+    return MongoDB.MongoClient.connect(config.db.host).then((client) => {
+        db = client.db("Xdb")
+    })
+    
+}
---
+++ router/auth.mjs
@@ -0,0 +1,17 @@
+import express from "express"
+
+const router = express.Router()
+
+// 회원 가입
+
+
+
+// 로그인
+
+
+
+// 로그인 유지 체크
+
+
+
+export default router
